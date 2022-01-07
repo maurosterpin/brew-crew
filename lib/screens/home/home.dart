87fe2967ee9fa68ce26767143cd5929466711321
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-01-07 10:46:09 +0100

    appbar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class Home extends StatelessWidget {
   const Home({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('home'),
+    return Scaffold(
+      backgroundColor: Colors.greenAccent[700],
+      appBar: AppBar(
+        title: Text('Kriya'),
+        backgroundColor: HexColor("#141518"),
+        actions: <Widget>[
+          FlatButton.icon(
+            onPressed: () {}, 
+            icon: Icon(Icons.person), 
+            label: Text(""),
+            
+            )
+        ]
+      ),
     );
   }
 }
