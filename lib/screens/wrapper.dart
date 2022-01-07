1c7cd16cf5b3799b8ce1e06adf65f5bed24e2e35
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-01-07 10:24:42 +0100

    auth initial data

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -10,7 +10,7 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final user = Provider.of<FUser>(context);
+    final user = Provider.of<FUser?>(context);
     print(user);
 
     // return either home or authenticate widget
