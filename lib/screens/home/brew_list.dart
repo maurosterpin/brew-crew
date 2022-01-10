4e9c075e9b7c67fd2a2adf45512f1c53bd3dc69c
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-01-10 21:44:02 +0100

    list iterable

--- lib/screens/home/brew_list.dart
+++ lib/screens/home/brew_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:provider/provider.dart';
 import 'package:brew_crew/models/brew.dart';
+import 'package:brew_crew/screens/home/brew_tile.dart';
 
 class BrewList extends StatefulWidget {
 
@@ -13,17 +14,13 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
 
-    final brews = Provider.of<Iterable<Brew?>?>(context);
+    final brews = Provider.of<List<Brew>?>(context);
     
-    brews?.forEach((brew) {
-      print(brew!.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
-
-    
-    return Container(
-      
-    );
+    return ListView.builder(
+      itemCount: brews?.length,
+      itemBuilder: (context, index) {
+        return BrewTile(brew: brews![index]);
+      },
+      );
   }
 }
--- lib/screens/home/brew_tile.dart
+++ lib/screens/home/brew_tile.dart
@@ -8,8 +8,19 @@ class BrewTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Padding(
+      padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0),
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 25.0,
+            backgroundColor: Colors.brown[brew.strength],
+          ),
+          title: Text(brew.name),
+          subtitle: Text('Takes ${brew.sugars} sugar(s)'),
+        ),
+      ),
     );
   }
 }
