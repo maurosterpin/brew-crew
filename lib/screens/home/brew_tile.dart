680d7c9bf8aa596e79256697b27289ebf5cec131
MSterpin <92162018+maurosterpin@users.noreply.github.com>
2022-01-10 21:16:26 +0100

    brew_tile.dart

---
+++ lib/screens/home/brew_tile.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:brew_crew/models/brew.dart';
+
+class BrewTile extends StatelessWidget {
+
+  final Brew brew;
+  BrewTile({ required this.brew});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
